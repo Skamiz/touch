afdf7733423a650fe9e38db637eb34530150e0d5
Skamiz <48406239+Skamiz@users.noreply.github.com>
2021-09-17 21:13:33 +0200

    remove touch object imediately when node is dug

--- init.lua
+++ init.lua
@@ -17,6 +17,10 @@ local MAX_LIGHT = tonumber(minetest.settings:get("touch_max_light")) or 1
 -- By default objects with 'visual = "item"' are too large so they need to be rescaled
 local scale = (2/3) + 0.001 -- +0.001 to avoid Z fighting
 
+-- tracking which nodes are currently touched to hopefully optimize
+-- glow removal on beeing dug
+local touched_nodes = {}
+
 minetest.register_entity(modname .. ":node_object", {
 	initial_properties = {
 		visual = "item",
@@ -36,6 +40,7 @@ minetest.register_entity(modname .. ":node_object", {
 		self.time = self.time + dtime
 
 		if self.time > GLOW_TIME then
+			touched_nodes[minetest.hash_node_position(self.object:get_pos())] = nil
 			self.object:remove()
 		end
 	end,
@@ -49,10 +54,26 @@ minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 	then
 		local node_obj = minetest.add_entity(pointed_thing.under, modname .. ":node_object")
 		node_obj:set_properties({wield_item = node.name})
-		
+
 		-- to avoid the touched node to be darker then before
 		if light_above > GLOW_STRENGTH then
 			node_obj:set_properties({glow = light_above})
 		end
+
+		touched_nodes[minetest.hash_node_position(pointed_thing.under)] = true
+	end
+end)
+
+minetest.register_on_dignode(function(pos, oldnode, digger)
+	node_index = minetest.hash_node_position(pos)
+	if touched_nodes[node_index] then
+		local objects = minetest.get_objects_in_area(pos, pos)
+		for _, v in pairs(objects) do
+			if not v:is_player() and v:get_luaentity().name == modname .. ":node_object" then
+				v:remove()
+			end
+		end
+
+		touched_nodes[node_index] = nil
 	end
 end)
