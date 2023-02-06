989f872a0cf9443df4e4188da75489615c64b571
Skamiz <48406239+Skamiz@users.noreply.github.com>
2023-02-06 12:28:29 +0100

    shape only implemented nodboxes now displaye by sepparate objects

--- init.lua
+++ init.lua
@@ -12,6 +12,8 @@ funfact: snow doesn't display correctly, the top surface is pusshed further down
 		when the node is scaled up (to avoid z-fighting)
 
 doesn't quite work with connected parts of connected nodeboxes
+
+TODO: wallmounted rotations
 ]]
 
 local GLOW_TIME = tonumber(minetest.settings:get("touch_glow_time")) or 5
@@ -21,7 +23,8 @@ local SHAPE_ONLY = minetest.settings:get_bool("touch_shape_only", false)
 
 
 -- By default objects with 'visual = "item"' are too large so they need to be rescaled
-local scale = (2/3) + 0.001 -- +0.001 to avoid Z fighting
+local item_scale = (2/3) + 0.001 -- +0.001 to avoid Z fighting
+local cube_scale = 1 + 0.001
 
 -- tracking which nodes are currently touched to hopefully optimize
 -- glow removal on beeing dug
@@ -29,14 +32,13 @@ local touched_nodes = {}
 
 minetest.register_entity(modname .. ":node_object", {
 	initial_properties = {
-		visual = "item",
-		visual_size = {x = scale, y = scale, z = scale},
+		visual = "cube",
 		physical = false,
 		pointable = false,
 		static_save = false,
 		glow = GLOW_STRENGTH,
 		shaded = false,
-		collisionbox = {-0.0, 0.0, -0.0, 0.0, 0.0, 0.0},
+		-- collisionbox = {-0.0, -0.0, -0.0, 0.0, 0.0, 0.0},
 	},
 	on_activate = function(self, staticdata, dtime_s)
 		self.time = 0
@@ -50,28 +52,16 @@ minetest.register_entity(modname .. ":node_object", {
 			self.object:remove()
 		end
 	end,
+
+	on_deactivate = function(self, removal)
+		for _, child in pairs(self.object:get_children() or {}) do
+			child:detach()
+			child:remove()
+		end
+	end
 })
 
--- local box_drawtypes = {
--- 	["normal"] = true,
--- 	["glasslike"] = true,
--- 	["glasslike_framed"] = true,
--- 	["glasslike_framed_optional"] = true,
--- 	["normal"] = true,
--- }
-local function spawn_touch_object(pos, node, glow)
-	local node_def = minetest.registered_nodes[node.name]
-	if not node_def then return false end
 
-	local node_obj = minetest.add_entity(pos, modname .. ":node_object")
-	node_obj:set_properties({wield_item = node.name, glow = glow})
-
-	if node_def and node_def.paramtype2 == "facedir" then
-		local rot = rotations.facedir[node.param2]
-		node_obj:set_rotation(rot)
-	end
-	return node_obj
-end
 local shape_texture = {
 	"touch_shape_only.png",
 	"touch_shape_only.png",
@@ -80,18 +70,104 @@ local shape_texture = {
 	"touch_shape_only.png",
 	"touch_shape_only.png",
 }
-local cube_scale = 1 + 0.001
-local function spawn_touch_shape_object(pos, node, light)
+
+local function nodebox(pos, node)
+	local node_def = minetest.registered_nodes[node.name]
+	local root_obj = minetest.add_entity(pos, modname .. ":node_object")
+	root_obj:set_properties({textures = {
+		"touch_empty.png",
+		"touch_empty.png",
+		"touch_empty.png",
+		"touch_empty.png",
+		"touch_empty.png",
+		"touch_empty.png",
+	}})
+	local boxes = node_def.node_box.fixed
+	if type(boxes[1]) == "number" then
+		boxes = {boxes}
+	end
+	for _, box in pairs(boxes) do
+		local box_obj = minetest.add_entity(pos, modname .. ":node_object")
+		local visual_size = {
+			x = box[4] - box[1] + 0.001,
+			y = box[5] - box[2] + 0.001,
+			z = box[6] - box[3] + 0.001,
+		}
+		local textures
+		if SHAPE_ONLY then
+			textures = table.copy(shape_texture)
+		else
+			textures = table.copy(node_def.tiles)
+			for i = 1, 6 do
+				if textures[i] and type(textures[i]) == "table" then
+					textures[i] = textures[i].name
+				end
+			end
+			for i = 2, 6 do
+				if not textures[i] then
+					textures[i] = textures[i-1]
+				end
+			end
+		end
+
+		-- Yes I know this it looks kind of janky, but it works correctly and that's all I need from it.
+		-- This cuts out the specific parts of the texture needed by the nodbox box.
+		-- It also probably crashes with nodeboxes which aren't pixel aligned
+		-- It also probably doesn't work with textures which aren't 16*16
+		textures[1] = "[combine:" .. (box[4] - box[1])*16 .. "x" .. (box[6] - box[3])*16 .. ":" .. (-box[1] - 0.5) * 16 .. "," .. ( box[6] - 0.5) * 16 .. "=" .. textures[1]
+		textures[2] = "[combine:" .. (box[4] - box[1])*16 .. "x" .. (box[6] - box[3])*16 .. ":" .. (-box[1] - 0.5) * 16 .. "," .. (-box[3] - 0.5) * 16 .. "=" .. textures[2]
+		textures[3] = "[combine:" .. (box[6] - box[3])*16 .. "x" .. (box[5] - box[2])*16 .. ":" .. (-box[3] - 0.5) * 16 .. "," .. ( box[5] - 0.5) * 16 .. "=" .. textures[3]
+		textures[4] = "[combine:" .. (box[6] - box[3])*16 .. "x" .. (box[5] - box[2])*16 .. ":" .. ( box[6] - 0.5) * 16 .. "," .. ( box[5] - 0.5) * 16 .. "=" .. textures[4]
+		textures[5] = "[combine:" .. (box[4] - box[1])*16 .. "x" .. (box[5] - box[2])*16 .. ":" .. ( box[4] - 0.5) * 16 .. "," .. ( box[5] - 0.5) * 16 .. "=" .. textures[5]
+		textures[6] = "[combine:" .. (box[4] - box[1])*16 .. "x" .. (box[5] - box[2])*16 .. ":" .. (-box[1] - 0.5) * 16 .. "," .. ( box[5] - 0.5) * 16 .. "=" .. textures[6]
+		box_obj:set_properties({visual_size = visual_size, textures = textures})
+
+
+		local attach_pos = {
+			x = (box[4] + box[1]) / 2 * 10,
+			y = (box[5] + box[2]) / 2 * 10,
+			z = (box[6] + box[3]) / 2 * 10,
+		}
+		box_obj:set_attach(root_obj, nil, attach_pos)
+	end
+	return root_obj
+end
+
+
+local function simple_cube(pos, node)
+	local node_obj = minetest.add_entity(pos, modname .. ":node_object")
+	if SHAPE_ONLY then
+		node_obj:set_properties({
+			visual = "cube",
+			textures = shape_texture,
+			visual_size = {x = cube_scale, y = cube_scale, z = cube_scale},
+		})
+	else
+		node_obj:set_properties({
+			visual = "item",
+			visual_size = {x = item_scale, y = item_scale, z = item_scale},
+			wield_item = node.name,
+			glow = glow,
+		})
+	end
+	return node_obj
+end
+
+local function spawn_touch_object(pos, node, glow)
 	local node_def = minetest.registered_nodes[node.name]
 	if not node_def then return false end
+	local node_obj
+	if node_def.drawtype == "nodebox" then
+		node_obj = nodebox(pos, node)
+	else
+		node_obj = simple_cube(pos, node, glow)
+	end
 
-	local node_obj = minetest.add_entity(pos, modname .. ":node_object")
-	node_obj:set_properties({
-		visual = "cube",
-		textures = shape_texture,
-		glow = glow,
-		visual_size = {x = cube_scale, y = cube_scale, z = cube_scale},
-	})
+
+	node_obj:set_properties({glow = glow})
+	for _, child in pairs(node_obj:get_children() or {}) do
+		child:set_properties({glow = glow})
+	end
 
 	if node_def and node_def.paramtype2 == "facedir" then
 		local rot = rotations.facedir[node.param2]
@@ -107,11 +183,7 @@ minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 			light_above <= MAX_LIGHT
 	then
 		local glow = math.max(light_above, GLOW_STRENGTH)
-		if not SHAPE_ONLY then
-			spawn_touch_object(pointed_thing.under, node, glow)
-		else
-			spawn_touch_shape_object(pointed_thing.under, node, glow)
-		end
+		spawn_touch_object(pointed_thing.under, node, glow)
 
 		touched_nodes[minetest.hash_node_position(pointed_thing.under)] = true
 	end
